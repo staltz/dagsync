e756bab8ba28329df0ae1723fcad8a7058f12134
Andre Staltz <andre@staltz.com>
2023-03-10 14:44:24 +0200

    init

---
+++ sliced-feeds.js
@@ -0,0 +1,71 @@
+const { BloomFilter } = require('bloom-filters')
+
+exports.name = 'dagsync'
+
+exports.manifest = {
+  getCommonRange: 'async',
+  getMessagesMissing: 'async',
+}
+
+exports.permissions = {
+  anonymous: {
+    allow: ['getCommonRange', 'getMessagesMissing'],
+  },
+}
+
+exports.init = function init(ssb, config) {
+  function getRangeOf(feedId) {
+    let minSeq = Infinity
+    let maxSeq = 0
+    ssb.db.forEach((msg) => {
+      if (msg.value.author === feedId) {
+        minSeq = Math.min(minSeq, msg.value.sequence)
+        maxSeq = Math.max(maxSeq, msg.value.sequence)
+      }
+    })
+    return [minSeq, maxSeq]
+  }
+
+  function getCommonRange(feedId, remoteRange, cb) {
+    const localRange = getRangeOf(feedId)
+    cb(null, [
+      Math.max(localRange[0], remoteRange[0]),
+      Math.max(localRange[1], remoteRange[1]),
+    ])
+  }
+
+  function calcBloom(feedId, range, iteration) {
+    const [minSeq, maxSeq] = range
+    const rangeSize = maxSeq - minSeq + 1
+    const filter = new BloomFilter(10 * rangeSize, 4) // TODO tweak params
+    ssb.db.forEach((msg) => {
+      const { author, sequence } = msg.value
+      if (author === feedId && sequence >= minSeq && sequence <= maxSeq) {
+        filter.add(iteration + msg.key)
+      }
+    })
+    return filter.saveAsJSON()
+  }
+
+  function getMessagesMissing(feedId, range, iteration, remoteBloomJSON, cb) {
+    const [minSeq, maxSeq] = range
+    const remoteFilter = BloomFilter.fromJSON(remoteBloomJSON)
+    const missing = []
+    ssb.db.forEach((msg) => {
+      const { author, sequence } = msg.value
+      if (author === feedId && sequence >= minSeq && sequence <= maxSeq) {
+        if (!remoteFilter.has(iteration + msg.key)) {
+          missing.push(msg.value)
+        }
+      }
+    })
+    cb(null, missing)
+  }
+
+  return {
+    getRangeOf,
+    getCommonRange,
+    calcBloom,
+    getMessagesMissing,
+  }
+}
---
+++ test/classic-sliced.js
@@ -0,0 +1,102 @@
+const test = require('tape')
+const ssbKeys = require('ssb-keys')
+const path = require('path')
+const os = require('os')
+const rimraf = require('rimraf')
+const SecretStack = require('secret-stack')
+const caps = require('ssb-caps')
+const p = require('util').promisify
+
+const ALICE_DIR = path.join(os.tmpdir(), 'dagsync-alice')
+const BOB_DIR = path.join(os.tmpdir(), 'dagsync-bob')
+
+rimraf.sync(ALICE_DIR)
+rimraf.sync(BOB_DIR)
+
+const createSSB = SecretStack({ appKey: caps.shs })
+  .use(require('ssb-memdb'))
+  .use(require('ssb-classic'))
+  .use(require('ssb-box'))
+  .use(require('../sliced-feeds'))
+
+test('replicate a sliced classic feed', async (t) => {
+  const alice = createSSB({
+    keys: ssbKeys.generate('ed25519', 'alice'),
+    path: ALICE_DIR,
+  })
+
+  const bob = createSSB({
+    keys: ssbKeys.generate('ed25519', 'bob'),
+    path: BOB_DIR,
+  })
+
+  await alice.db.loaded()
+  await bob.db.loaded()
+
+  const carolKeys = ssbKeys.generate('ed25519', 'carol')
+  const carolMsgs = []
+  const carolID = carolKeys.id
+  for (let i = 1; i <= 10; i++) {
+    const msg = await p(alice.db.create)({
+      feedFormat: 'classic',
+      content: { type: 'post', text: 'm' + i },
+      keys: carolKeys,
+    })
+    carolMsgs.push(msg)
+  }
+  t.pass('alice has msgs 1..10 from carol')
+
+  await p(bob.db.add)(carolMsgs[5].value)
+  await p(bob.db.add)(carolMsgs[6].value)
+  await p(bob.db.add)(carolMsgs[7].value)
+
+  {
+    const arr = bob.db
+      .filterAsArray((msg) => msg.value.author === carolID)
+      .map((msg) => msg.value.content.text)
+    t.deepEquals(arr, ['m6', 'm7', 'm8'], 'bob has msgs 6..8 from carol')
+  }
+
+  const remoteAlice = await p(bob.connect)(alice.getAddress())
+  t.pass('bob connected to alice')
+
+  // Manual dag-sync steps
+  const rangeAtBob = bob.dagsync.getRangeOf(carolID)
+  const commonRange = await p(remoteAlice.dagsync.getCommonRange)(
+    carolID,
+    rangeAtBob
+  )
+  const bobGot = new Map()
+  for (let i = 0; i < 4; i++) {
+    const bloom = bob.dagsync.calcBloom(carolID, commonRange, i)
+    const missingIter = await p(remoteAlice.dagsync.getMessagesMissing)(
+      carolID,
+      commonRange,
+      i,
+      bloom
+    )
+    for (const msgVal of missingIter) {
+      bobGot.set(msgVal.sequence, msgVal)
+    }
+  }
+  const missingForBob = [...bobGot.values()].sort((a, b) => a.sequence - b.sequence)
+  for (const msgVal of missingForBob) {
+    await p(bob.db.add)(msgVal)
+  }
+  t.pass('bob got messages via dagsync')
+
+  {
+    const arr = bob.db
+      .filterAsArray((msg) => msg.value.author === carolID)
+      .map((msg) => msg.value.content.text)
+    t.deepEquals(
+      arr,
+      ['m6', 'm7', 'm8', 'm9', 'm10'],
+      'bob has msgs 6..10 from carol'
+    )
+  }
+
+  await p(remoteAlice.close)(true)
+  await p(alice.close)(true)
+  await p(bob.close)(true)
+})
