dd6f1e992dcb5dcc78ff9be3370bda7b7aa8549c
Andre Staltz <andre@staltz.com>
2023-03-14 17:51:28 +0200

    support sliced replication that deletes the past

--- test/feed-sync.js
+++ test/feed-sync.js
@@ -79,3 +79,73 @@ test('sync a sliced classic feed', async (t) => {
   await p(alice.close)(true)
   await p(bob.close)(true)
 })
+
+test('delete old msgs and sync latest msgs', async (t) => {
+  const ALICE_DIR = path.join(os.tmpdir(), 'dagsync-alice')
+  const BOB_DIR = path.join(os.tmpdir(), 'dagsync-bob')
+
+  rimraf.sync(ALICE_DIR)
+  rimraf.sync(BOB_DIR)
+
+  const alice = createSSB({
+    keys: ssbKeys.generate('ed25519', 'alice'),
+    path: ALICE_DIR,
+  })
+
+  const bob = createSSB({
+    keys: ssbKeys.generate('ed25519', 'bob'),
+    path: BOB_DIR,
+    feedSync: {
+      limit: 3,
+    }
+  })
+
+  await alice.db.loaded()
+  await bob.db.loaded()
+
+  const carolKeys = ssbKeys.generate('ed25519', 'carol')
+  const carolMsgs = []
+  const carolID = carolKeys.id
+  for (let i = 1; i <= 10; i++) {
+    const msg = await p(alice.db.create)({
+      feedFormat: 'classic',
+      content: { type: 'post', text: 'm' + i },
+      keys: carolKeys,
+    })
+    carolMsgs.push(msg)
+  }
+  t.pass('alice has msgs 1..10 from carol')
+
+  await p(bob.db.add)(carolMsgs[5].value)
+  await p(bob.db.add)(carolMsgs[6].value)
+  await p(bob.db.add)(carolMsgs[7].value)
+
+  {
+    const arr = bob.db
+      .filterAsArray((msg) => msg?.value.author === carolID)
+      .map((msg) => msg.value.content.text)
+    t.deepEquals(arr, ['m6', 'm7', 'm8'], 'bob has msgs 6..8 from carol')
+  }
+
+  const remoteAlice = await p(bob.connect)(alice.getAddress())
+  t.pass('bob connected to alice')
+
+  bob.feedSync.request(carolID)
+  await p(setTimeout)(1000)
+  t.pass('feedSync!')
+
+  {
+    const arr = bob.db
+      .filterAsArray((msg) => msg?.value.author === carolID)
+      .map((msg) => msg.value.content.text)
+    t.deepEquals(
+      arr,
+      ['m8', 'm9', 'm10'],
+      'bob has msgs 8..10 from carol'
+    )
+  }
+
+  await p(remoteAlice.close)(true)
+  await p(alice.close)(true)
+  await p(bob.close)(true)
+})
